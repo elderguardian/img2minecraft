331acd4f720f5bc606b5f28eb31810c7b491b76a
elderguardian <katzenmaedchen@tutanota.com>
2023-05-26 23:26:08 +0200

    made image scale down if its too large

--- js/mapArt.js
+++ js/mapArt.js
@@ -1,16 +1,39 @@
-const resizeOnCanvas = async (imageFile, width, height) => {
+const scaleImageSize = (width, height, maxWidth, maxHeight) => {
+    let newSize = {width, height}
+    let ratio = 0
+
+    if (width > maxWidth) {
+        ratio = maxWidth / width
+
+        newSize.width = maxWidth
+        newSize.height = height * ratio
+    }
+
+    if (height > maxHeight) {
+        ratio = maxHeight / height
+
+        newSize.height = maxHeight
+        newSize.width = width * ratio
+    }
+
+    return newSize
+}
+
+const imageOnSmallCanvas = async (imageFile, maxWidth, maxHeight) => {
 
     //wait for image to load using promise, then put on canvas and resize
-    const imageOnCanvas = (imageFile, width, height) =>  new Promise(resolve => {
+    const imageOnCanvas = imageFile => new Promise(resolve => {
         const canvas = document.createElement('canvas')
         const image = new Image()
         image.src = window.webkitURL.createObjectURL(imageFile)
-        image.onload = () => resolve({ image, canvas })
+        image.onload = () => resolve({image, canvas})
     })
 
-    const {canvas,image} = await imageOnCanvas(imageFile, width, height)
-    canvas.width = width
-    canvas.height = height
+    const {canvas, image} = await imageOnCanvas(imageFile)
+
+    const newImageSize = scaleImageSize(image.width, image.height, maxWidth, maxHeight)
+    canvas.width = newImageSize.width
+    canvas.height = newImageSize.height
 
     const canvasContext = canvas.getContext('2d')
     canvasContext.drawImage(image, 0, 0, canvas.width, canvas.height)
